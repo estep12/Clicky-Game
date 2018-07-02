c93569c2407b059d9113b0a073ee47f5180036ec
Erik Stephenson <33075989+estep12@users.noreply.github.com>
2018-07-01 21:41:28 -0500

    Added navbar and header

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import Nav from './components/Nav'
+import Header from './components/Header'
+import LostCards from './components/LostCards'
+import cards from "./cards.json"
 import './App.css';
 
+
+
 class App extends Component {
+
+  state = {
+    picked: [],
+    correct: 0,
+    topscore: 0,
+    message: 'Click an image to begin'
+  };
+
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Nav></Nav>
+        <Header></Header>
       </div>
     );
   }
