fd52fb1dcad44ddd3ae842a2703278037886a60c
Erik Stephenson <33075989+estep12@users.noreply.github.com>
2018-07-05 21:06:46 -0500

    Added styling to images. Nav component works as well as registering clicks

--- src/App.js
+++ src/App.js
@@ -1,27 +1,48 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import Nav from './components/Nav'
-import Header from './components/Header'
+// import Header from './components/Header'
 import LostCards from './components/LostCards'
 import cards from "./cards.json"
 import './App.css';
 
-
+const clicked = [];
 
 class App extends Component {
 
   state = {
     cards: cards,
-    picked: [],
     correct: 0,
-    topscore: 0
+    topScore: 0,
+    clicked: false,
+    headline: "Click and image to begin!"
   };
+
+  imageClick = (event) => {
+    event.preventDefault;
+    if(this.state.clicked === false) {
+    console.log(event.target.key)
+    this.setState({headline: "You Guessed Correctly!"});
+    this.setState({score: this.state.correct +1});
+    this.setState({topScore: this.state.topScore +1})
+    this.setState({clicked: true});
+
+    }
+    else {
+      this.setState({headline: "You Guessed Incorrectly"})
+    }
+  }
 
 
   render() {
     return (
       <div>
-        <Nav></Nav>
+        <Nav
+         correct={this.state.correct}
+         topScore={this.state.topScore}
+         headline={this.state.headline}
+         />
+
         <main className="container">
         {this.state.cards.map((cards) => (
           <div key={cards.id} className="image-holder" onClick={this.imageClick} clicked={this.state.clicked}>
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-const Nav = (props) => (
+const Nav = props => (
     <nav className = "navbar navbar-light bg-primary">
         <span className="navbar-brand mb-0 h1">Clicky Game</span>
-        <span className="text-center mb-0 h3">Click an image to begin!</span>
+        <span className="text-center mb-0 h3">{props.headline}</span>
         <span className="navbar-text text-dark font-weight-bold">
-          Score: <span>{props.correct}</span>&nbsp;
-          Top Score: <span>{props.topscore}</span>
+          Score:  <span className="badge">{props.correct}</span> | 
+          Top Score:  <span className="badge">{props.topScore}</span>
         </span>
     </nav>
 )
